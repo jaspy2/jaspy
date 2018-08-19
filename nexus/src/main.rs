528a083d822941488e10bb1ed46dc8b7573f7d2a
Antti Tönkyrä <daedalus@pingtimeout.net>
2018-08-19 19:38:04 +0300

    Initial IMDS worker logic

--- nexus/src/main.rs
+++ nexus/src/main.rs
@@ -14,15 +14,39 @@ mod db;
 mod schema;
 mod utilities;
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::AtomicBool;
+
+fn should_continue(running : &std::sync::atomic::AtomicBool) -> bool {
+    return running.load(std::sync::atomic::Ordering::Relaxed);
+}
+
+fn imds_worker(running : Arc<AtomicBool>, imds : Arc<Mutex<utilities::imds::IMDS>>) {
+    loop {
+        if !should_continue(&running) { break; }
+        // get devices & ports
+        {
+            // refresh results :)
+            if let Ok(ref mut imds) = imds.lock() {
+                imds.refresh_device(&"test.fqdn".to_string());
+                imds.refresh_interface(&"test.fqdn".to_string(), 1234, &"interfacePort1".to_string(), false, None);
+                imds.refresh_interface(&"test.fqdn".to_string(), 123, &"interfacePort2".to_string(), false, None);
+            };
+        }
+
+        // TODO: optimize: after we get initial result, we don't need to refresh unless something changes
+        std::thread::sleep(std::time::Duration::from_millis(1000));
+    }
+}
 
 fn main() {
+    let running = Arc::new(AtomicBool::new(true));
     let imds : Arc<Mutex<utilities::imds::IMDS>> = Arc::new(Mutex::new(utilities::imds::IMDS::new()));
 
-    if let Ok(ref mut imds) = imds.lock() {
-        imds.refresh_device(&"test.fqdn".to_string());
-        imds.refresh_interface(&"test.fqdn".to_string(), 1234, &"interfacePort1".to_string(), false, None);
-        imds.refresh_interface(&"test.fqdn".to_string(), 123, &"interfacePort2".to_string(), false, None);
-    };
+    let imds_worker_imds = imds.clone();
+    let imds_worker_running = running.clone();
+    let imds_worker_thread = std::thread::spawn(|| {
+        imds_worker(imds_worker_running, imds_worker_imds);
+    });
     
     rocket::ignite()
         .mount(
@@ -48,4 +72,7 @@ fn main() {
         .manage(db::connect())
         .manage(imds.clone())
         .launch();
+
+    (*running).store(false, std::sync::atomic::Ordering::Relaxed);
+    imds_worker_thread.join().unwrap();
 }
