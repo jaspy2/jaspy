50d6e6a208a70db4f35c93752bcca667eeccb58f
Antti Tönkyrä <daedalus@pingtimeout.net>
2019-02-24 19:17:47 +0200

    Add basic debian packaging :)

---
+++ debian/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+tmpdir=$(mktemp -d)
+echo ${tmpdir}
+which diesel || cargo install diesel_cli
+mkdir -p ${tmpdir}/usr/lib/jaspy
+mkdir -p ${tmpdir}/var/lib/jaspy
+mkdir -p ${tmpdir}/var/lib/jaspy/nexus
+cp -a $(which diesel) ${tmpdir}/usr/lib/jaspy/diesel
+for item in nexus poller pinger snmptrapd-reader; do
+    pushd ../${item}
+    cargo build --release
+    cp -a target/release/jaspy-${item} ${tmpdir}/usr/lib/jaspy/
+    popd
+done
+cp -a ../discover ${tmpdir}/usr/lib/jaspy/
+cp -a ../snmpbot/mibs ${tmpdir}/var/lib/jaspy/
+cp -a ../weathermap ${tmpdir}/var/lib/jaspy/
+cp -a ../nexus/migrations ${tmpdir}/var/lib/jaspy/nexus/
+fpm --force -t deb -s dir -n "jaspy" -v "2.0.0" -m "Antti Tönkyrä <daedalus@pingtimeout.net>" --after-remove ./postremove.sh --after-install ./postinst.sh systemd/system/=/etc/systemd/system/ ${tmpdir}/=/
+rm -rf ${tmpdir}
---
+++ debian/postremove.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+id jaspy && userdel jaspy || true
+
