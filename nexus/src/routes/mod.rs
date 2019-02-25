def65896aab9a73287235b622ba11abf5891cf1b
Joona Romppanen <joona.romppanen@gmail.com>
2019-02-25 19:02:35 +0200

    Mark clientlocation and weathermap for separation from nexus

--- nexus/src/routes/mod.rs
+++ nexus/src/routes/mod.rs
@@ -1,6 +1,6 @@
-pub mod clientlocation;
+pub mod clientlocation; // TODO: Separate to standalone service
 pub mod discovery;
 pub mod device;
 pub mod metrics;
 pub mod interface;
-pub mod weathermap;
+pub mod weathermap; // TODO: Separate to standalone service
