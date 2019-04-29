48cda0c4841a474f471f7e8aefe09fa7dd581594
Joona Romppanen <joona.romppanen@gmail.com>
2019-04-29 23:09:44 +0300

    Remove device ref from route mod

--- nexus/src/routes/mod.rs
+++ nexus/src/routes/mod.rs
@@ -1,6 +1,5 @@
 pub mod clientlocation; // TODO: Separate to standalone service
 pub mod discovery;
-pub mod device;
 pub mod metrics;
 pub mod interface;
 pub mod weathermap; // TODO: Separate to standalone service
