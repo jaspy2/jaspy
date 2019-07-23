ce41c4ada947ea23b488b64b8fb663407348af80
Antti Tönkyrä <daedalus@pingtimeout.net>
2019-07-23 16:35:17 +0300

    add method to GET clientlocation information

--- nexus/src/routes/dev/clientlocation.rs
+++ nexus/src/routes/dev/clientlocation.rs
@@ -4,6 +4,15 @@ use db;
 use rocket_contrib::json;
 
 // TODO: GH#9 Move everything to v1 API
+#[get("/?<client_address>")]
+pub fn get_clientlocation(connection: db::Connection, client_address: String) -> Option<json::Json<models::dbo::ClientLocation>> {
+    if let Some(existing_client_info) = models::dbo::ClientLocation::by_ip(&client_address, &connection) {
+        return Some(json::Json(existing_client_info));
+    } else {
+        return None;
+    }
+}
+
 #[put("/", data = "<client_location_info_json>")]
 pub fn put_clientlocation(connection: db::Connection, client_location_info_json: json::Json<models::json::ClientLocationInfo>) {
     let client_location_info: models::json::ClientLocationInfo = client_location_info_json.into_inner();
