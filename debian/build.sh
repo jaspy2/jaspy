eb6d61ec683ce57b873bafc5d5d5a956891f665b
Antti Jaakkola <annttu@git.annttu.fi>
2019-08-02 14:49:04 +0300

    Add  systemd service

--- debian/build.sh
+++ debian/build.sh
@@ -18,6 +18,7 @@ for item in entitypoller; do
     ./build.linux
     popd
 done
+cp -a ../entitypoller/build/entitypoller-linux ${tmpdir}/usr/local/bin/jaspy-entitypoller
 cp -a ../discover ${tmpdir}/usr/lib/jaspy/
 cp -a ../cli/* ${tmpdir}/usr/local/bin/
 cp -a ../snmpbot/mibs ${tmpdir}/var/lib/jaspy/
