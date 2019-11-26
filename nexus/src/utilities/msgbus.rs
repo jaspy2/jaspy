3b2c556ed28195f11c050e745d47968c5da3118a
Antti Tönkyrä <daedalus@pingtimeout.net>
2019-11-26 16:20:59 +0200

    Messagebus case when MQTT is not set, closes #26

--- nexus/src/utilities/msgbus.rs
+++ nexus/src/utilities/msgbus.rs
@@ -4,7 +4,7 @@ use rumqtt::{MqttClient, MqttOptions, QoS, ReconnectOptions};
 extern crate serde_json;
 
 pub struct MessageBus {
-    mqtt_client: rumqtt::MqttClient,
+    mqtt_client: Option<rumqtt::MqttClient>,
 }
 
 impl MessageBus {
@@ -16,28 +16,37 @@ impl MessageBus {
                 event_publish = env_opt.clone();
             },
             Err(_) => {
-                panic!("JASPY_EVENT_PUBLISH env var not set!");
+                return MessageBus {
+                    mqtt_client: None
+                };
             }
         }
 
-        let reconnection_options = ReconnectOptions::Always(10);
+        let reconnection_options = ReconnectOptions::AfterFirstSuccess(1);
         let mqtt_options = MqttOptions::new("jaspy-nexus", event_publish, 1883)
             .set_keep_alive(10)
             .set_reconnect_opts(reconnection_options)
             .set_clean_session(false);
 
-        let (mqtt_client, _notifications) = MqttClient::start(mqtt_options).unwrap();
+        let mqtt_client;
+        if let Ok(mqtt_start_result) = MqttClient::start(mqtt_options) {
+            mqtt_client = mqtt_start_result.0;
+        } else {
+            panic!("failed initial connection to MQTT server, if MQTT server is not needed, unset JASPY_MQTT_SERVER");
+        }
 
         return MessageBus {
-            mqtt_client: mqtt_client,
+            mqtt_client: Some(mqtt_client),
         };
     }
 
 
     pub fn event(self: &mut MessageBus, event: models::events::Event) {
-        let json_data = format!("{}", json!(event));
-        if let Ok(_) = self.mqtt_client.publish(format!("jaspy/nexus/{}", event.event_type), QoS::AtLeastOnce, false, json_data) {
-            // ..
+        if let Some(mqtt_client) = &mut self.mqtt_client {
+            let json_data = format!("{}", json!(event));
+            if let Ok(_) = mqtt_client.publish(format!("jaspy/nexus/{}", event.event_type), QoS::AtLeastOnce, false, json_data) {
+                // ..
+            }
         }
     }
 }
